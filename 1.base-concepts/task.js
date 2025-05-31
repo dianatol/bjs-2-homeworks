518cf3ad080f52a4fb16ae32f378de4a674b178b
Working Git <dianatoluz@gmail.com>
2025-05-31 12:58:42 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  let arr = []
+  const discriminant = b**2 - 4*a*c;
+  if(discriminant < 0) {
+    return arr
+  }
+  else if(discriminant > 0) {
+    const x1 = (-b + Math.sqrt(discriminant))/(2*a);
+    const x2 = (-b - Math.sqrt(discriminant))/(2*a);
+    arr.push(x1,x2)
+  }
+  else if (discriminant === 0) {
+    const x = -b/(2*a)
+    arr.push(x)
+  }
   return arr;
 }
 
