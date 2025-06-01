e6ce0cb9ec84f9cb681638f49063ff17c1c434ef
Working Git <dianatoluz@gmail.com>
2025-06-01 12:31:05 +0300

    Исправлены недочеты по названию переменных и кодстайлу в первом задании

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,26 +2,21 @@
 function solveEquation(a, b, c) {
   let arr = []
   const discriminant = b**2 - 4*a*c;
-  if(discriminant < 0) {
-    return arr
-  }
-  else if(discriminant > 0) {
-    const x1 = (-b + Math.sqrt(discriminant))/(2*a);
-    const x2 = (-b - Math.sqrt(discriminant))/(2*a);
-    arr.push(x1,x2)
-  }
-  else if (discriminant === 0) {
-    const x = -b/(2*a)
-    arr.push(x)
-  }
-  return arr;
+  if(discriminant > 0) {
+    const firstRadical = (-b + Math.sqrt(discriminant))/(2*a);
+    const secondRadical = (-b - Math.sqrt(discriminant))/(2*a);
+    arr.push(firstRadical,secondRadical)
+  } else if (discriminant === 0) {
+    const radical = -b/(2*a)
+    arr.push(radical)
+  } return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  const P = (percent/100)/12;
+  const monthlyInterestRate = (percent/100)/12;
   const credit = amount - contribution; 
-  let every_month = credit * (P + (P / (Math.pow(1 + P, countMonths) - 1))) 
-  let itog_summ = every_month*countMonths;
-  return Number(itog_summ.toFixed(2))
+  let monthlyLoanPayment = credit * (monthlyInterestRate + (monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1))) 
+  let totalAmountCredit = monthlyLoanPayment*countMonths;
+  return Number(totalAmountCredit.toFixed(2))
 }
