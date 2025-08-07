9eed3d13ea89cf7a8057e3e106011b850e35f815
Working Git <dianatoluz@gmail.com>
2025-08-07 22:50:29 +0300

    Шестое задание выполнено полностью

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,58 @@
 ﻿
+function parseCount(value) {
+  const parsed = Number.parseFloat(value);
+  if (Number.isNaN(parsed)) {
+    throw new Error("Невалидное значение");
+  }
+  return parsed;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+class Triangle {
+    constructor(sideOne, sideTwo, sideThree) {
+    if (sideOne + sideTwo <= sideThree || sideOne + sideThree <= sideTwo || sideTwo + sideThree <= sideOne) {
+        throw new Error("Треугольник с такими сторонами не существует")
+       }  
+    this.sideOne = sideOne;
+    this.sideTwo = sideTwo;
+    this.sideThree = sideThree;
+    }
+
+    get perimeter() {
+        return this.sideOne+this.sideTwo+this.sideThree;
+    }
+
+    get area() {
+        let p = (this.sideOne + this.sideTwo + this.sideThree) / 2;
+        const area = Math.sqrt(p*(p-this.sideOne)*(p-this.sideTwo)*(p-this.sideThree));
+        return Number(area.toFixed(3));
+        
+    } 
+}
+
+function getTriangle(sideOne, sideTwo, sideThree) {
+  try {
+    return new Triangle(sideOne, sideTwo, sideThree);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
+
+
+
+
+
